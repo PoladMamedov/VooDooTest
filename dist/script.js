f75c6bf704c672845e3fb46904e9ebb8e2258c50
Polad Mamedov <poladikksp@gmail.com>
2023-08-02 23:16:22 +0300

    little code reorganization

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,110 @@
+//! products list fethcing and pagination setup
+async function getProducts(page) {
+  try {
+    const response = await fetch(`https://voodoo-sandbox.myshopify.com/products.json?page=${page}`);
+    if (response.ok) {
+      const products = await response.json();
+      return products;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function createProductItem(item) {
+  const { title, variants, images } = item;
+  const listItem = document.createElement("li");
+  listItem.className = "text-sm flex flex-col gap-3 justify-between max-w-lg relative overflow-hidden";
+  listItem.innerHTML = `<div class="p-2 bg-black rounded-md text-white text-xs absolute top-3 left-3">USED</div>
+  <img class="p-3 border border-black rounded-md h-80 w-full object-cover" src=${images[0]?.src} alt="item">
+  <div class="flex justify-between">
+      <div class="font-bold">
+        <p>${title}</p>
+        <p>${variants[0].price} UAH</p>
+      </div>
+      <div>
+      <p class="text-end font-bold">condition</p>
+      <p class="text-end">slightly used</p>
+      </div>
+  </div>
+  <button class="addToCartBtn p-4 bg-black rounded-md text-white hover:bg-gray-700">ADD TO CART</button>
+  <div id="adding-confirm" class="w-full flex justify-center items-center absolute transition-all -bottom-10">
+    <span class="bg-green-500 text-white text-2xl w-10 h-10 rounded-full flex justify-center items-center">&#10003;</span>
+  </div>
+  `;
+  listItem.addEventListener("click", (e) => {
+    if (e.target.classList.contains("addToCartBtn")) addToCart(listItem, item);
+  });
+  return listItem;
+}
+
+async function appendProducts(list, page) {
+  const { products } = await getProducts(page);
+  const items = products.map((item) => {
+    return createProductItem(item);
+  });
+  items.forEach((item) => {
+    list.appendChild(item);
+  });
+}
+
+function createPaginationAndList(totalPages, page) {
+  productList.innerHTML = "";
+  appendProducts(productList, page);
+  let liTag = "";
+  const liTagClasses =
+    "flex justify-center items-center p-3 w-11 h-11 border border-black rounded-full text-lg cursor-pointer hover:bg-black hover:text-white";
+  let active;
+  let beforePage = page - 1;
+  let afterPage = page + 1;
+
+  if (page > 2) {
+    liTag += `<li class="${liTagClasses}" onclick="createPaginationAndList(totalPages, 1)">1</li>`;
+    if (page > 3) {
+      liTag += `<li class="p-3">...</li>`;
+    }
+  }
+  if (page == totalPages) {
+    beforePage = beforePage - 2;
+  } else if (page == totalPages - 1) {
+    beforePage = beforePage - 1;
+  }
+  if (page == 1) {
+    afterPage = afterPage + 2;
+  } else if (page == 2) {
+    afterPage = afterPage + 1;
+  }
+  for (let plength = beforePage; plength <= afterPage; plength++) {
+    if (plength > totalPages) {
+      continue;
+    }
+    if (plength == 0) {
+      plength = plength + 1;
+    }
+    if (page == plength) {
+      active = "bg-black text-white";
+    } else {
+      active = "";
+    }
+    liTag += `<li class="${liTagClasses} ${active}" onclick="createPaginationAndList(totalPages, ${plength})">${plength}</li>`;
+  }
+  if (page < totalPages - 1) {
+    if (page < totalPages - 2) {
+      liTag += `<li class="p-3">...</li>`;
+    }
+    liTag += `<li class="${liTagClasses}" onclick="createPaginationAndList(totalPages, ${totalPages})">${totalPages}</li>`;
+  }
+
+  paginationBlock.innerHTML = liTag;
+  return liTag;
+}
+
+const productList = document.querySelector("#products-list");
+const paginationBlock = document.querySelector(".pagination ul");
+let totalPages = 16;
+let page = 1;
+paginationBlock.innerHTML = createPaginationAndList(totalPages, page);
+
 //! cart opening/closing
 const cartOpenBtn = document.querySelector("#cart-open-btn");
 const cartCloseBtn = document.querySelector("#cart-close-btn");
@@ -123,110 +230,3 @@ function decreaseQuantity(item) {
 const cartItemsList = document.querySelector("#cart-items-list");
 let cartItems = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
 appendCartItems(cartItemsList);
-
-//! products list fethcing and pagination setup
-async function getProducts(page) {
-  try {
-    const response = await fetch(`https://voodoo-sandbox.myshopify.com/products.json?page=${page}`);
-    if (response.ok) {
-      const products = await response.json();
-      return products;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-function createProductItem(item) {
-  const { title, variants, images } = item;
-  const listItem = document.createElement("li");
-  listItem.className = "text-sm flex flex-col gap-3 justify-between max-w-lg relative overflow-hidden";
-  listItem.innerHTML = `<div class="p-2 bg-black rounded-md text-white text-xs absolute top-3 left-3">USED</div>
-  <img class="p-3 border border-black rounded-md h-80 w-full object-cover" src=${images[0]?.src} alt="item">
-  <div class="flex justify-between">
-      <div class="font-bold">
-        <p>${title}</p>
-        <p>${variants[0].price} UAH</p>
-      </div>
-      <div>
-      <p class="text-end font-bold">condition</p>
-      <p class="text-end">slightly used</p>
-      </div>
-  </div>
-  <button class="addToCartBtn p-4 bg-black rounded-md text-white hover:bg-gray-700">ADD TO CART</button>
-  <div id="adding-confirm" class="w-full flex justify-center items-center absolute transition-all -bottom-10">
-    <span class="bg-green-500 text-white text-2xl w-10 h-10 rounded-full flex justify-center items-center">&#10003;</span>
-  </div>
-  `;
-  listItem.addEventListener("click", (e) => {
-    if (e.target.classList.contains("addToCartBtn")) addToCart(listItem, item);
-  });
-  return listItem;
-}
-
-async function appendProducts(list, page) {
-  const { products } = await getProducts(page);
-  const items = products.map((item) => {
-    return createProductItem(item);
-  });
-  items.forEach((item) => {
-    list.appendChild(item);
-  });
-}
-
-function createPaginationAndList(totalPages, page) {
-  productList.innerHTML = "";
-  appendProducts(productList, page);
-  let liTag = "";
-  const liTagClasses =
-    "flex justify-center items-center p-3 w-11 h-11 border border-black rounded-full text-lg cursor-pointer hover:bg-black hover:text-white";
-  let active;
-  let beforePage = page - 1;
-  let afterPage = page + 1;
-
-  if (page > 2) {
-    liTag += `<li class="${liTagClasses}" onclick="createPaginationAndList(totalPages, 1)">1</li>`;
-    if (page > 3) {
-      liTag += `<li class="p-3">...</li>`;
-    }
-  }
-  if (page == totalPages) {
-    beforePage = beforePage - 2;
-  } else if (page == totalPages - 1) {
-    beforePage = beforePage - 1;
-  }
-  if (page == 1) {
-    afterPage = afterPage + 2;
-  } else if (page == 2) {
-    afterPage = afterPage + 1;
-  }
-  for (let plength = beforePage; plength <= afterPage; plength++) {
-    if (plength > totalPages) {
-      continue;
-    }
-    if (plength == 0) {
-      plength = plength + 1;
-    }
-    if (page == plength) {
-      active = "bg-black text-white";
-    } else {
-      active = "";
-    }
-    liTag += `<li class="${liTagClasses} ${active}" onclick="createPaginationAndList(totalPages, ${plength})">${plength}</li>`;
-  }
-  if (page < totalPages - 1) {
-    if (page < totalPages - 2) {
-      liTag += `<li class="p-3">...</li>`;
-    }
-    liTag += `<li class="${liTagClasses}" onclick="createPaginationAndList(totalPages, ${totalPages})">${totalPages}</li>`;
-  }
-
-  paginationBlock.innerHTML = liTag;
-  return liTag;
-}
-
-const productList = document.querySelector("#products-list");
-const paginationBlock = document.querySelector(".pagination ul");
-let totalPages = 16;
-let page = 1;
-paginationBlock.innerHTML = createPaginationAndList(totalPages, page);
